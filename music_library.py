a6d0497295a71a97259c8049ded349700e3f502b
Kailash Aghav <kailashaghav1354@gmail.com>
2025-11-22 12:21:41 +0530

    First upload of Cherry AI

---
+++ music_library.py
@@ -0,0 +1,7 @@
+music= {
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI&list=RDDeumyOzKqgI&start_radio=1",
+    "heartless": "https://www.youtube.com/watch?v=rbb-_YU2S4c&list=RDrbb-_YU2S4c&start_radio=1",
+    "starboy" : "https://www.youtube.com/watch?v=34Na4j8AVgA&list=RD34Na4j8AVgA&start_radio=1",
+    "not like us" : "https://www.youtube.com/watch?v=T6eK-2OQtew&list=RDT6eK-2OQtew&start_radio=1"
+    
+}
